a90442f1628d44f3abbdd52cd1673556b1716198
vtanh2 <vtanh2@cmc.com.vn>
2021-06-15 15:53:49 +0700

    lesson18-Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -5,25 +5,26 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs);
-  }
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        console.log(data);
-        setBlogs(data)
-      });
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000)
   }, []);
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 }
